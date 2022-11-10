10bc808997f87952d767e625a8a0ccc143d61501
Haruka08 <haruka.hashimoto08@gmail.com>
2022-11-10 16:20:25 +0000

    updated routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getThoughts)
+    .get(getSingleThought)
+    .post(createThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+  .route('/:videoId')
+  .post(createReaction)
+  .delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router
+    .route('/')
+    .get(getUsers)
+    .post(createUser)
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post(createFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
