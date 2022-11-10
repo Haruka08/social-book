4cbca7cb35fbfdf861fc01a173a48b1df6f2a7cd
Haruka08 <haruka.hashimoto08@gmail.com>
2022-11-10 15:48:18 +0000

    created folders and files required and created models

---
+++ models/Thought.js
@@ -0,0 +1,50 @@
+const { ObjectId } = require('bson');
+const { Schema, model } = require('mongoose');
+
+// Schema to create Reaction schema
+const reactionSchema = new Schema(
+    {
+      reactionId: ObjectId,
+      reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      createdAt: { type: Date, default: Date.now },
+    }
+  );
+
+// Schema to create Thought schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+      },
+    createdAt: { type: Date, default: Date.now },
+    username: {
+        type: String,
+        required: true
+      },
+    reactions: [reactionSchema],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Initialize thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
